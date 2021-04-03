b862531033d0f52a103782adf5a1ed6cdda99a11
StefanM3000 <stefanmilenkovicbusiness@gmail.com>
2021-04-03 22:18:12 +0200

    debug initial render state

--- src/components/details/index.jsx
+++ src/components/details/index.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 const Details = (props) => {
-  const [hero, setHero] = useState([]);
+  const [hero, setHero] = useState(null);
   const id = props.match.params.id;
   const url = `https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/id/${id}.json`;
 
@@ -12,9 +12,18 @@ const Details = (props) => {
   }, [url]);
 
   console.log(hero);
+
+  if (!hero) return null;
+
   return (
     <div>
+      <div className="flex justify-center items-center">
+        <img src={hero.images.lg} alt={hero.name} className="rounded "></img>
+      </div>
+      <br></br>
       <div>Name: {hero.name}</div>
+      <div>Power: {hero.powerstats.power}</div>
+      
     </div>
   );
 };
