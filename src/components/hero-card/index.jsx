2db17d1e857670dff9cccbdc16bfe59a19a6f6d0
StefanM3000 <stefanmilenkovicbusiness@gmail.com>
2021-04-03 22:56:33 +0200

    debug name text

--- src/components/hero-card/index.jsx
+++ src/components/hero-card/index.jsx
@@ -12,7 +12,7 @@ const HeroCard = () => {
         <Link key={item.id} to={`/superhero/${item.id}`}>
           <div className=" justify-center items-center flex flex-col p-4 bg-black shadow-2xl rounded-2xl border-2 border-white-1">
             <img src={item.images.sm} alt={item.name} className="rounded" />
-            <p className="p-3">{item.name}</p>
+            <p className="p-3 text-white">{item.name}</p>
           </div>
         </Link>
       ))}
