3d5b7e5d919e7b0e2279b867c22ed39eb7767870
StefanM3000 <stefanmilenkovicbusiness@gmail.com>
2021-04-11 18:26:48 +0200

    add fav number to nav

--- src/components/navbar/index.jsx
+++ src/components/navbar/index.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+import { useObserver } from "mobx-react";
+import { useHeroesStore } from "../../mobx/heroes-context/HeroesContext";
+
 const Navbar = () => {
+  const heroesStore = useHeroesStore();
+  const favsNumber = useObserver(() => heroesStore.heroes.length);
+
   return (
     <header className="flex flex-row items-baseline justify-evenly bg-black border-b p-6 pb-0 md:pb-4 fixed w-full z-10 top-0">
       <div className="flex items-center justify-between mb-4 ">
@@ -14,10 +20,18 @@ const Navbar = () => {
       <div className="flex items-center justify-between mb-4 ">
         <h1 className="leading-none text-2xl text-white">
           <Link
-            className="no-underline text-white hover:text-blue-500"
+            className="no-underline text-white hover:text-blue-500 flex"
             to="/favs"
           >
-            favs
+            favs{"  "}
+            <div className="relative bg-white">
+              <span
+                className="font-semibold text-xs w-4 absolute top-0 left-1 text-center bg-white rounded-full text-black
+            "
+              >
+                {favsNumber}
+              </span>
+            </div>
           </Link>
         </h1>
       </div>
