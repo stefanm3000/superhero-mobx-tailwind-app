b4e8a2acf9d6a40926561142a134adb6a9b58f0e
StefanM3000 <stefanmilenkovicbusiness@gmail.com>
2021-04-12 11:01:30 +0200

    add postcss plugin for style jsx w/ tailwind

--- src/components/stats-graph/index.jsx
+++ src/components/stats-graph/index.jsx
@@ -2,21 +2,27 @@ import React from "react";
 
 const Graph = ({ stat, heroes, skill }) => {
   return (
-    <li className="relative p-1">
-      <span className="flex-start">{skill}: </span>
-      <span className="absolute right-2 font-semibold">
-        {(stat / heroes).toFixed(2)}
-      </span>
-      <span className="bg-gradient-to-r from-red-400 to-green-400 h-100 w-4/5 left-28 absolute">
-        &nbsp;
-        <span
-          className={`${skill} h-6 rounded left-30% absolute border w-1 bg-white`}
-          style={{ left: `${stat / heroes}%` }}
-        >
-          &nbsp;
+    <>
+      <li className="relative p-1">
+        <span className="flex-start">{skill}: </span>
+        <span className="absolute right-2 font-semibold">
+          {(stat / heroes).toFixed(2)}
         </span>
-      </span>
-    </li>
+        <span className="bg-gradient-to-r from-red-400 to-green-400 h-100 w-4/5 left-28 absolute">
+          &nbsp;
+          <span
+            className={`${skill} h-6 rounded left-30% absolute border w-1 bg-white`}
+          >
+            &nbsp;
+          </span>
+        </span>
+      </li>
+      <style jsx>{`
+        .${skill} {
+          left: ${stat / heroes}%;
+        }
+      `}</style>
+    </>
   );
 };
 
