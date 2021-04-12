10970f4324b932001941a04abb2cc6431891a746
StefanM3000 <stefanmilenkovicbusiness@gmail.com>
2021-04-12 11:52:26 +0200

    update indicator to css triangle

--- src/components/stats-graph/index.jsx
+++ src/components/stats-graph/index.jsx
@@ -1,21 +1,16 @@
 import React from "react";
 
 const Graph = ({ stat, heroes, skill }) => {
-
   const number = parseInt(stat / heroes);
-  
+
   return (
     <>
       <li className="relative p-1">
-        <span className="flex-start">{skill}: </span>
-        <span className="absolute right-2 font-semibold">
-          {(stat / heroes).toFixed(2)}
-        </span>
-        <span className="bg-gradient-to-r from-red-400 to-green-400 h-100 w-4/5 left-28 absolute">
+        <span className="flex-start font-semibold">{skill}: </span>
+        <span className="absolute right-2 font-semibold">{number}</span>
+        <span className="bg-gradient-to-r from-red-400 to-green-400 h-100 w-4/5 left-28 rounded absolute">
           &nbsp;
-          <span
-            className={`leftperc-${number} h-6 rounded left-30% absolute border w-1 bg-white`}
-          >
+          <span className={`leftperc-${number} arrow-up bottom-0 absolute w-1`}>
             &nbsp;
           </span>
         </span>
@@ -23,6 +18,5 @@ const Graph = ({ stat, heroes, skill }) => {
     </>
   );
 };
-
 
 export default Graph;
