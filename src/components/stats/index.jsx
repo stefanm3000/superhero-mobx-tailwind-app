1d5e7ffd4a9e8d164cd8440f2045b58ab95de496
StefanM3000 <stefanmilenkovicbusiness@gmail.com>
2021-04-12 17:50:45 +0200

    improve legibility and syntax

--- src/components/stats/index.jsx
+++ src/components/stats/index.jsx
@@ -8,19 +8,19 @@ const Stats = ({ heroes }) => {
   const heroesJS = toJS(observableHeroes);
   const numOfFavs = observableHeroes.length;
 
-  let powernum = 0;
-  let strengthnum = 0;
-  let intellinum = 0;
-  let speednum = 0;
-  let combatnum = 0;
+  let powerNum = 0;
+  let strengthNum = 0;
+  let intelliNum = 0;
+  let speedNum = 0;
+  let combatNum = 0;
 
   const powerstat = () =>
     heroesJS.forEach((hero) => {
-      powernum = powernum + hero.powerstats.power;
-      strengthnum = strengthnum + hero.powerstats.strength;
-      intellinum = intellinum + hero.powerstats.intelligence;
-      speednum = speednum + hero.powerstats.speed;
-      combatnum = combatnum + hero.powerstats.combat;
+      powerNum += hero.powerstats.power;
+      strengthNum += hero.powerstats.strength;
+      intelliNum += hero.powerstats.intelligence;
+      speedNum += hero.powerstats.speed;
+      combatNum += hero.powerstats.combat;
     });
 
   powerstat();
@@ -29,11 +29,11 @@ const Stats = ({ heroes }) => {
     <div className="flex flex-col mt-6">
       <h1 className="text-3xl p-4">Your favorite heroes' average stats: </h1>
       <ul className="border w-4/5 self-center p-6 text-left rounded">
-        <Graph stat={powernum} heroes={numOfFavs} skill="Power" />
-        <Graph stat={strengthnum} heroes={numOfFavs} skill="Strength" />
-        <Graph stat={intellinum} heroes={numOfFavs} skill="Intelligence" />
-        <Graph stat={speednum} heroes={numOfFavs} skill="Speed" />
-        <Graph stat={combatnum} heroes={numOfFavs} skill="Combat" />
+        <Graph stat={powerNum} heroes={numOfFavs} skill="Power" />
+        <Graph stat={strengthNum} heroes={numOfFavs} skill="Strength" />
+        <Graph stat={intelliNum} heroes={numOfFavs} skill="Intelligence" />
+        <Graph stat={speedNum} heroes={numOfFavs} skill="Speed" />
+        <Graph stat={combatNum} heroes={numOfFavs} skill="Combat" />
       </ul>
     </div>
   );
