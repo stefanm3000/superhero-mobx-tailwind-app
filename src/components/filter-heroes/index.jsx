7c40f827d9ae30906d3049f1ff28795d1e9da525
StefanM3000 <stefanmilenkovicbusiness@gmail.com>
2021-04-13 17:01:27 +0200

    add state for inputs

--- src/components/filter-heroes/index.jsx
+++ src/components/filter-heroes/index.jsx
@@ -2,41 +2,57 @@ import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const FilterHeroes = () => {
-  const StatInput = ({ stat }) => {
-    const [num, setNum] = useState(50);
+  const [heroStats, setHeroStats] = useState({
+    power: 50,
+    strength: 50,
+    speed: 50,
+    intelligence: 50,
+    combat: 50,
+  });
 
+  console.log("heroStats", heroStats);
+
+  const StatInput = ({ powerstat }) => {
     const handleChange = (e) => {
-      setNum(e.target.value);
+      setHeroStats((prevState) => ({
+        ...prevState,
+        [e.target.name]: parseInt(e.target.value),
+      }));
     };
+
+    console.log("heroStats.powerstat", heroStats.powerstat);
 
     return (
       <div className="flex flex-col border rounded p-3 px-6 my-4 bg-gray-200 relative">
-        <label htmlFor={`${stat}`} className="font-semibold">
-          {stat}
+        <label htmlFor={`${powerstat}`} className="font-semibold">
+          {powerstat}
         </label>
         <input
-          id={`${stat}Id`}
+          id={powerstat}
+          name={powerstat}
           type="range"
           className="rounded-lg overflow-hidden appearance-none bg-gray-400 h-3 w-128 outline-none"
           min="1"
           max="100"
           step="1"
+          value={heroStats.powerstat}
           onChange={handleChange}
         ></input>
         <div className="absolute right-4 top-1 font-semibold text-xs w-8 h-5 text-center bg-pink-400 rounded-full border text-black">
-          {num}
+          {heroStats.powerstat}
         </div>
       </div>
     );
   };
+
   return (
     <div>
-      <div className="flex m-auto flex-col w-4/5 border rounded p-4 transparent">
-        <StatInput stat="power" />
-        <StatInput stat="strength" />
-        <StatInput stat="speed" />
-        <StatInput stat="intelligence" />
-        <StatInput stat="combat" />
+      <div className="flex m-auto flex-col border rounded p-4 transparent ">
+        <StatInput powerstat="power" />
+        <StatInput powerstat="strength" />
+        <StatInput powerstat="speed" />
+        <StatInput powerstat="intelligence" />
+        <StatInput powerstat="combat" />
         <Link
           to="/filtered"
           className="m-auto font-bold text-xl border rounded p-6 w-40 bg-white text-blue-600"
