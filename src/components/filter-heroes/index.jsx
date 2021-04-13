ed3550a51319cbf2b506f0da9a0f3cdf9f94f6c5
StefanM3000 <stefanmilenkovicbusiness@gmail.com>
2021-04-13 17:35:33 +0200

    debug1

--- src/components/filter-heroes/index.jsx
+++ src/components/filter-heroes/index.jsx
@@ -10,52 +10,81 @@ const FilterHeroes = () => {
     combat: 50,
   });
 
-  console.log("heroStats", heroStats);
-
-  const StatInput = ({ powerstat }) => {
-    const [num, setNum] = useState(50);
-
-    const handleChange = (e) => {
-      setNum(parseInt(e.target.value));
-    };
-
-    return (
-      <div className="flex flex-col border rounded p-3 px-6 my-4 bg-gray-200 relative">
-        <label htmlFor={`${powerstat}`} className="font-semibold">
-          {powerstat}
-        </label>
-        <input
-          id={powerstat}
-          name={powerstat}
-          type="range"
-          className="rounded-lg overflow-hidden appearance-none bg-gray-400 h-3 w-128 outline-none"
-          min="1"
-          max="100"
-          step="1"
-          value={num}
-          onChange={handleChange}
-        ></input>
-        <div className="absolute right-4 top-1 font-semibold text-xs w-8 h-5 text-center bg-pink-400 rounded-full border text-black">
-          {num}
-        </div>
-      </div>
-    );
+  const onChange = (stat, value) => {
+    setHeroStats({
+      ...heroStats,
+      [stat]: value,
+    });
   };
 
   return (
     <div>
       <div className="flex m-auto flex-col border rounded p-4 transparent ">
-        <StatInput powerstat="power" />
-        <StatInput powerstat="strength" />
-        <StatInput powerstat="speed" />
-        <StatInput powerstat="intelligence" />
-        <StatInput powerstat="combat" />
+        <StatInput
+          key="power"
+          onChange={onChange}
+          stat="power"
+          value={heroStats.power}
+        />
+        <StatInput
+          key="strength"
+          onChange={onChange}
+          stat="strength"
+          value={heroStats.strength}
+        />
+        <StatInput
+          key="speed"
+          onChange={onChange}
+          stat="speed"
+          value={heroStats.speed}
+        />
+        <StatInput
+          key="intelligence"
+          onChange={onChange}
+          stat="intelligence"
+          value={heroStats.intelligence}
+        />
+        <StatInput
+          key="combat"
+          onChange={onChange}
+          stat="combat"
+          value={heroStats.combat}
+        />
         <Link
           to="/filtered"
           className="m-auto font-bold text-xl border rounded p-6 w-40 bg-white text-blue-600"
         >
           filter
         </Link>
+      </div>
+    </div>
+  );
+};
+
+const StatInput = ({ onChange, stat, value }) => {
+  const handleChange = () => {
+    onChange(stat, value);
+  };
+
+  console.log(stat, value);
+
+  return (
+    <div className="flex flex-col border rounded p-3 px-6 my-4 bg-gray-200 relative">
+      <label htmlFor={stat} className="font-semibold">
+        {stat}
+      </label>
+      <input
+        id={stat}
+        name={stat}
+        type="range"
+        className="rounded-lg overflow-hidden appearance-none bg-gray-400 h-3 w-128 outline-none"
+        min="1"
+        max="100"
+        step="1"
+        onChange={handleChange}
+      ></input>
+      <div className="absolute right-4 top-1 font-semibold text-xs w-8 h-5 text-center bg-pink-400 rounded-full border text-black">
+        {value}
       </div>
     </div>
   );
